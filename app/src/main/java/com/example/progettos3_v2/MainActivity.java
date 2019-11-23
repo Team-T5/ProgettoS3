01c6130defb62755479b0a7f770c6862463122e8
Gabriele Colapinto <gabriele.colapinto@gmail.com>
2019-11-23 20:28:47 +0100

    Schermata di Login e parte della schermata di registrazione

--- app/src/main/java/com/example/progettos3_v2/MainActivity.java
+++ app/src/main/java/com/example/progettos3_v2/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import io.realm.Realm;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +13,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Login(){
         Realm realm = Realm.getDefaultInstance();
         try{
-            //Inserire il codice qui
+            //Prelevo i valori delle caselle di testo e della checkbox
+            EditText editUsername, editPassword;
+            CheckBox checkRicordami;
+
+            String strUsername, strPassword;
+            Boolean Ricordami;
+
+            editUsername = findViewById(R.id.editUsername);
+            editPassword = findViewById(R.id.editPassword);
+            checkRicordami = findViewById(R.id.checkRicordami);
+
+            strUsername = editUsername.getText();
+            strPassword = editPassword.getText();
+            if(checkRicordami.isChecked()){
+                Ricordami = true;
+            }
+            else{
+                Ricordami = false;
+            }
+
+            /*
+            Inserire la query qui
+             */
+
+
+            /*
+            Se la query ha avuto successo e la checkbox è stata spuntata bisogna inserire i dati in una sessione
+             */
+
+            /*
+            Passare alla schermata principale dell'app
+             */
 
         } catch( Error e){
             e.getMessage();
--- app/src/main/java/com/example/progettos3_v2/Registrazione.java
+++ app/src/main/java/com/example/progettos3_v2/Registrazione.java
@@ -15,18 +15,8 @@ public class Registrazione extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registrazione);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
+
+
 
 }
