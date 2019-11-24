7ae7e8201bb397402310940da462dba5527a87f0
Gabriele Colapinto <gabriele.colapinto@gmail.com>
2019-11-24 20:03:19 +0100

    Sviluppo dele interfacce di base realizzabili senza l'accesso al database

---
+++ app/src/main/java/com/example/progettos3_v2/singolaCategoria.java
@@ -0,0 +1,34 @@
+package com.example.progettos3_v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class singolaCategoria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singola_categoria);
+
+        /*
+        La query di prelievo delle materie deve essere all'interno dell' onCreate perché quando
+        la schermata si apre i dati devono essere già disponibili.
+
+        La selezione di una voce dell'elenco che non sia vuota è un evento che causa la comparsa
+        degli elementi della categoria.
+
+        Selezionata anche la categoria compare il pulsante.
+
+        Pseudo codice:
+        Materia.onChanege(){
+                Se materia == null  //L'utente potrebe selezionare di nuovo null
+                        Impota categoria e pulsante invisibili e non cliccabili
+                Altrimenti
+                        Esegui la query per prelevare le categorie correlate alla materia
+                        e stampa lo spinner delle cagegorie
+
+        Fare lo stesso ragionamento con le voci di Categoria e con il pulsante
+         */
+    }
+}
---
+++ app/src/main/java/com/example/progettos3_v2/statisticheEsercizi.java
@@ -0,0 +1,27 @@
+package com.example.progettos3_v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class statisticheEsercizi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistiche_esercizi);
+    }
+
+    public void gotoSingolaCategoria(View view){
+        /*
+        Inserire l'intent
+         */
+    }
+
+    public void gotoStrutturaEsame(View view){
+        /*
+        Inserire l'intent
+         */
+    }
+}
